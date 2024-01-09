70f5d690da5ced2bcb8daeca9301e46af969ce9b
Marry <maria.halamova@whalebone.io>
2024-01-09 10:56:10 +0100

    Vue template init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <NavBar></NavBar>
+  </header>
+
+  <RouterView class="bg-white dark:bg-gray-900" />
+</template>
+
+<script setup lang="ts">
+import NavBar from '@/components/NavBar.vue'
+import { RouterView } from 'vue-router'
+</script>
+
+<style scoped>
+
+</style>
